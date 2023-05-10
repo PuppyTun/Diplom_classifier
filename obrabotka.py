ca92ef2996d3bf567a5ed97a790c022866b3443f
PuppyTun <balamba2000@list.ru>
2023-05-11 02:46:15 +0500

    Поставил пробел

--- obrabotka.py
+++ obrabotka.py
@@ -53,7 +53,6 @@ x.index = df.index
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
-
 loaded_model = pickle.load(open("SGDClassifier.sav", 'rb'))
 
 prediction = ['dhrrhdrjjrd rshhsrsr']
